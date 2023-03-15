f04a503e95c7dac38dd420c1d48f0357dbbf8bea
vincenttommi <vincenttommi@gmail.com>
2023-03-15 07:34:28 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+const currentUser  = 'Grace Hopper';
+
+
+const welcomeMessage = 'Welcome to Flatbook, + Grace Hopper!';
+
+
+const excitedWelcomeMessage  = 'WELCOME TO FLATBOOK, + GRACE HOPPER!';
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
